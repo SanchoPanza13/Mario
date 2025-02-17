c7fc3f9b7cb439387bae44de717e6b86545c6c7f
SanchoPanza13 <wayan.el.sanchez13@gmail.com>
2025-02-17 18:56:03 +0100

    ACABADO
    Juego acabado, falta comentar código

--- Assets/Scripts/MenuBehaviour.cs
+++ Assets/Scripts/MenuBehaviour.cs
@@ -1,18 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MenuBehaviour : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    
+    public void LoadMenu()
     {
-        
+        SceneManager.LoadScene("Menu");
     }
 
-    // Update is called once per frame
-    void Update()
+    public void LoadGame()
     {
-        
+        SceneManager.LoadScene("SampleScene");
     }
+
 }
